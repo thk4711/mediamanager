8b6685cbfd066bfb74d9211a4da8aaa369bb896e
Thomas <thk4711@gmail.com>
2022-01-14 16:35:39 +0100

    Update start_installer.sh

--- installer/start_installer.sh
+++ installer/start_installer.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
-echo Starting media manager setup
-echo This will take up to 25min
-apt -y install git
-mkdir /install
-cd /install
-git clone https://github.com/thk4711/mediamanager
-ln -s mediamanager/ manager
-cd mediamanager/installer/
-./install_prerequisite.py
+echo '#--------------------------------#'
+echo '#                                #'
+echo '# Starting media manager setup   #'
+echo '#  This will take up to 25min.   #'
+echo '#                                #'
+echo '#--------------------------------#'
+sleep 5
+#echo This will take up to 25min
+#apt -y install git
+#mkdir /install
+#cd /install
+#git clone https://github.com/thk4711/mediamanager
+#ln -s mediamanager/ manager
+#cd mediamanager/installer/
+#./install_prerequisite.py
 echo 
-echo #--------------------------------#
-echo #                                #
-echo #    Please reboot now !!!       #
-echo #                                #
-echo #--------------------------------#
+echo '#--------------------------------#'
+echo '#                                #'
+echo '#    Please reboot now !!!       #'
+echo '#                                #'
+echo '#--------------------------------#'
 
